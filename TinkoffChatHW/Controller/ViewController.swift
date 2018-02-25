bafc40c59a08bf2c35a2412aa47b8b07b79a3c97
Georgy <fesenko.g@gmail.com>
2018-02-25 21:12:35 +0300

    second commit

--- TinkoffChatHW/Controller/ViewController.swift
+++ TinkoffChatHW/Controller/ViewController.swift
@@ -10,84 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var prevState = ""
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let state = convertState(state: UIApplication.shared.applicationState)
-        if state != prevState {
-            print("Application moved from \(prevState) to \(state): \(#function)")
-        } else {
-            print("Application is still in \(state): \(#function)")
-        }
-        prevState = state
+        print("Called function is \(#function)")
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        let state = convertState(state: UIApplication.shared.applicationState)
-        if state != prevState {
-            print("Application moved from \(prevState) to \(state): \(#function)")
-        } else {
-            print("Application is still in \(state): \(#function)")
-        }
-        prevState = state
+        print("Called function is \(#function)")
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let state = convertState(state: UIApplication.shared.applicationState)
-        if state != prevState {
-            print("Application moved from \(prevState) to \(state): \(#function)")
-        } else {
-            print("Application is still in \(state): \(#function)")
-        }
-        prevState = state
+        print("Called function is \(#function)")
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        let state = convertState(state: UIApplication.shared.applicationState)
-        if state != prevState {
-            print("Application moved from \(prevState) to \(state): \(#function)")
-        } else {
-            print("Application is still in \(state): \(#function)")
-        }
-        prevState = state
+        print("Called function is \(#function)")
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        let state = convertState(state: UIApplication.shared.applicationState)
-        if state != prevState {
-            print("Application moved from \(prevState) to \(state): \(#function)")
-        } else {
-            print("Application is still in \(state): \(#function)")
-        }
-        prevState = state
+        print("Called function is \(#function)")
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        let state = convertState(state: UIApplication.shared.applicationState)
-        if state != prevState {
-            print("Application moved from \(prevState) to \(state): \(#function)")
-        } else {
-            print("Application is still in \(state): \(#function)")
-        }
-        prevState = state
+        print("Called function is \(#function)")
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        let state = convertState(state: UIApplication.shared.applicationState)
-        if state != prevState {
-            print("Application moved from \(prevState) to \(state): \(#function)")
-        } else {
-            print("Application is still in \(state): \(#function)")
-        }
-        prevState = state
+        print("Called function is \(#function)")
     }
 }
 
