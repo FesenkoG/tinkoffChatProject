4f7e2df98c85d45b8a0d960e6d3012b2441bc83d
Georgy <fesenko.g@gmail.com>
2018-04-12 09:51:03 +0300

    seventh homework

---
+++ TinkoffChatHW/Services/StorageManager.swift
@@ -0,0 +1,9 @@
+//
+//  StorageManager.swift
+//  TinkoffChatHW
+//
+//  Created by Георгий Фесенко on 10.04.2018.
+//  Copyright © 2018 Georgy. All rights reserved.
+//
+
+import Foundation
---
+++ TinkoffChatHW/Utilities/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  TinkoffChatHW
+//
+//  Created by Георгий Фесенко on 10.04.2018.
+//  Copyright © 2018 Georgy. All rights reserved.
+//
+
+import Foundation
