64ad943ff461143fa5505beb0ad33f97c2e86967
Georgy <fesenko.g@gmail.com>
2018-05-27 14:49:03 +0300

    tests added

--- TinkoffChatHWTests/ProfileServiceTests.swift
+++ TinkoffChatHWTests/ProfileServiceTests.swift
@@ -90,4 +90,67 @@ class ProfileServiceTests: XCTestCase {
         
     }
     
+    func testDeleteMethodInService() {
+        
+        //given
+        let userInApp = UserInApp(name: "Ivan", descr: "Very good person", image: UIImage(named: "placeholder-user")!)
+        var name: String?
+        var descr: String?
+        let expectation = self.expectation(description: "Waiting for retrieve")
+        profileService.saveData(user: userInApp) { (success) in
+            //when
+            self.profileService.deleteUserData(completionHandler: { (success) in
+                if success {
+                    self.profileService.retrieveData(completionHandler: { (result) in
+                        switch result {
+                        case .Failure(let error):
+                            print(error)
+                        case .Success(let user):
+                            name = user.name
+                            descr = user.descr
+                        }
+                        expectation.fulfill()
+                        
+                    })
+                }
+            })
+        }
+        
+        waitForExpectations(timeout: 10, handler: nil)
+        //then
+        XCTAssertEqual(name!, "")
+        XCTAssertEqual(descr!, "")
+    }
+    
+    func testDeleteMethodInViewController() {
+        controllerUnderTest.usernameTxtField.text = "Vasya"
+        controllerUnderTest.userDescriptionTxtField.text = "Another good person"
+        controllerUnderTest.saveData()
+        
+        var name: String?
+        var description: String?
+        let expectation = self.expectation(description: "Waiting")
+        
+        //when
+        controllerUnderTest.deleteData()
+        controllerUnderTest.profileModel.retrieveData { (result) in
+            switch result {
+            case .Failure(let error):
+                print(error)
+            case .Success(let user):
+                name = user.name
+                description = user.descr
+                expectation.fulfill()
+            }
+            
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        //then
+        XCTAssertEqual(controllerUnderTest.usernameTxtField.text, "")
+        XCTAssertEqual(controllerUnderTest.userDescriptionTxtField.text, "")
+        XCTAssertEqual(name!, "")
+        XCTAssertEqual(descr!, "")
+    }
+    
 }
